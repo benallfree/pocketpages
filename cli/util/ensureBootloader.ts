75272c169e9a5f5df8548476cefe765236ba26e7
Ben Allfree <ben@benallfree.com>
2024-09-09 02:07:47 -0700

    wip

--- cli/util/ensureBootloader.ts
+++
@@ -1,9 +0,0 @@
-import { copyFileSync } from 'fs'
-import { join } from 'path'
-import { CLI_ROOT } from '..'
-
-export const ensureBootloader = () => {
-  const sourcePath = CLI_ROOT('..', 'lib', '+boot.pb.js')
-  const destinationPath = join('app', '+pocketpages.pb.js')
-  copyFileSync(sourcePath, destinationPath)
-}
