d6306a4911a442687dccbc0af18d3148f5afd829
Ben Allfree <ben@benallfree.com>
2025-01-17 19:30:49 -0800

    enh: improve require() error messaging

---
+++ .changeset/warm-trees-exist.md
@@ -0,0 +1,5 @@
+---
+'pocketpages': patch
+---
+
+Enh: improve require() error messaging
--- src/lib/pages/providers/v23Provider/index.ts
+++ src/lib/pages/providers/v23Provider/index.ts
@@ -4,10 +4,48 @@ export const v23Provider = (): IPagesProvider => ({
   boot: () => {
     onBootstrap((e) => {
       e.next()
+      // Only override require once
+      if (!(require as any).isOverridden) {
+        const oldRequire = require
+        require = (path) => {
+          try {
+            return oldRequire(path)
+          } catch (e) {
+            const errorMsg = `${e}`
+            if (errorMsg.includes('Invalid module')) {
+              throw new Error(
+                `${path} is not a valid module. Did you mean resolve()?`
+              )
+            }
+            throw e
+          }
+        }
+        // Set the flag on the new require function
+        ;(require as any).isOverridden = true
+      }
       require(`${__hooks}/pocketpages.pb`).AfterBootstrapHandler()
     })
 
     routerUse((e) => {
+      // Only override require once
+      if (!(require as any).isOverridden) {
+        const oldRequire = require
+        require = (path) => {
+          try {
+            return oldRequire(path)
+          } catch (e) {
+            const errorMsg = `${e}`
+            if (errorMsg.includes('Invalid module')) {
+              throw new Error(
+                `${path} is not a valid module. Did you mean resolve()?`
+              )
+            }
+            throw e
+          }
+        }
+        // Set the flag on the new require function
+        ;(require as any).isOverridden = true
+      }
       return require(`${__hooks}/pocketpages.pb`).v23MiddlewareWrapper(e)
     })
   },
