ed97bafe8b8fc2131ef9cc8f669eb39845d23e86
Rodrigo Moyle <rodrigorm@gmail.com>
2025-04-27 21:24:16 +0000

    Add page to test middleware execution order

---
+++ packages/site/pb_hooks/pages/(tests)/middlewares/+middleware.js
@@ -0,0 +1,7 @@
+/** @type {import('pocketpages').MiddlewareLoaderFunc} */
+module.exports = function (api) {
+    const start = new Date().toISOString();
+    sleep(250);
+
+    return { middlewares: [...(api.data.middlewares ?? []), `${start}: /+middleware.js`] }
+}
